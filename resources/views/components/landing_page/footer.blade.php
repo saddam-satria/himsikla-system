85a009f0964ed0cb00a40ed3e06ce1d4f1075acd
saddam-satria <ardhisaddam@gmail.com>
2022-09-21 11:14:16 +0700

    update ig link

--- resources/views/components/landing_page/footer.blade.php
+++ resources/views/components/landing_page/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <strong class="text-white">Copyright &copy; 2022 <a href="#" class="text-decoration-none">HIMSI KLA</a>.</strong>
         <div class="d-flex">
-            <span class="text-white"><i class="fab fa-instagram text-lg"></i></span>
+            <a target="__blank" href="https://instagram.com/himsi.kaliabang" class="text-white"><i class="fab fa-instagram text-lg"></i></a>
         </div>
     </div>
 </nav>
