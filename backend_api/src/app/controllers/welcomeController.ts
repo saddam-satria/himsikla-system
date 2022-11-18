33aaa12ca1edda266656f7f9301894199301eb68
saddam-satria <ardhisaddam@gmail.com>
2022-11-18 21:46:04 +0700

    update welcome messages

--- backend_api/src/app/controllers/welcomeController.ts
+++ backend_api/src/app/controllers/welcomeController.ts
@@ -1,26 +1,13 @@
 import { Request, Response } from 'express';
-import {
-  BASE_URL,
-  PORT,
-  BASE_ASSET,
-  BASE_PATH,
-  ASSET_URL,
-} from '../config/utils/constant';
 
 class WelcomeController {
   public get(_request: Request, response: Response) {
     response.status(200).json({
-      message: 'welcome to server node TS boilerplate',
+      message: 'Selamat Datang Di API HIMSI KLA',
       status: 'success',
-      owner: 'Saddam Satria',
-      github: 'https://github.com/saddam-satria',
-      linkedIn: 'https://www.linkedin.com/in/saddam-satria-ardhi-837570170/',
-      instagram: 'https://instagram.com/saddamsatria_12',
-      BASE_URL,
-      PORT,
-      BASE_PATH,
-      ASSET_URL,
-      BASE_ASSET,
+      owner: 'Divisi Pendidikan HIMSI KLA',
+      github: 'https://github.com/HIMSI-KALIABANG',
+      instagram: 'https://instagram.com/himsi.kaliabang',
     });
   }
 }
