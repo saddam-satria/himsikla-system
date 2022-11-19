e4f09d1b5bbe4eb409e21cad65f1d01558faa2b4
saddam-satria <ardhisaddam@gmail.com>
2022-11-19 20:09:35 +0700

    update cors

--- backend_api/src/server.ts
+++ backend_api/src/server.ts
@@ -11,7 +11,11 @@ import prisma from './app/config/prisma';
   dotenv.config();
   const app = express();
 
-  app.use(cors());
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+    })
+  );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(logging);
