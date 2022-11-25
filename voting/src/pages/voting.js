6a83eb7a2d44c2462fae740e2299a1bb1a9a2dfb
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 <ardhisaddam@gmail.com>
2022-11-25 10:18:35 +0700

    update prettier

--- voting/src/pages/voting.js
+++ voting/src/pages/voting.js
@@ -7,22 +7,18 @@ import getVoter from '../redux/action/voter/getVoter';
 import getVoters from '../redux/action/voter/getVoters';
 import postVoter from '../redux/action/voter/postVoter';
 
-
 function Voting() {
   const candidateState = useSelector((state) => state.candidate);
   const userState = useSelector((state) => state.user);
   const voterState = useSelector((state) => state.voter);
 
- 
-  const [currentVoter,setCurrentVoter] = React.useState(null);
-
+  const [currentVoter, setCurrentVoter] = React.useState(null);
 
   React.useEffect(() => {
-    if(voterState.voter){
-      setCurrentVoter(voterState.voter)
+    if (voterState.voter) {
+      setCurrentVoter(voterState.voter);
     }
-  },[voterState])
-
+  }, [voterState]);
 
   const render = React.useRef(true);
 
@@ -50,106 +46,101 @@ function Voting() {
     };
   }, [dispatch, voterState]);
 
-
-
-
-
-  const voteHandler = (e,candidate) => {
+  const voteHandler = (e, candidate) => {
     e.preventDefault();
     const date = new Date(Date.now());
     const currentUser = userState.data;
-    dispatch(getVoter(currentUser.member.name))
+    dispatch(getVoter(currentUser.member.name));
 
-
-    
     const payload = {
-      candidate, 
+      candidate,
       token: currentUser.member.token,
       name: currentUser.member.name,
       email: currentUser.email,
-      occupation : currentUser.member.occupation,
+      occupation: currentUser.member.occupation,
       periode: currentUser.member.periode,
       createdAt: `${date.getDate()}-${
         date.getMonth() + 1
       }-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
       updatedAt: null,
-    }
-
+    };
 
     dispatch(postVoter(payload));
-  }
+  };
 
   const voterGroupByCandidate = React.useMemo(() => {
     const candidates = {};
-    if(voterState.data){
-      voterState.data.map((voter) => {
-        candidates[voter.candidate] = 0;
-        return voter;
-      }).reduce((acc,item) =>{
-        candidates[item.candidate] += 1
-      },0 )
-    } 
-    return candidates
-  } ,[voterState])
+    if (voterState.data) {
+      voterState.data
+        .map((voter) => {
+          candidates[voter.candidate] = 0;
+          return voter;
+        })
+        .reduce((acc, item) => {
+          candidates[item.candidate] += 1;
+        }, 0);
+    }
+    return candidates;
+  }, [voterState]);
 
+  console.log(voterGroupByCandidate);
   return (
     <div>
-       {candidateState.loading || voterState.loading && (
-        <div className="fixed left-0 top-0 z-10 h-screen w-screen">
-          <div className="flex flex-col space-y-2 justify-center h-full w-full items-center">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL2tq0IANwwvpD-dJ-YD8Zbe0Xeriw2h-mdw&usqp=CAU"
-              alt="circle loading"
-              className="w-36 h-36 object-contain"
-            />
-            <span className="text-sm capitalize">tunggu sebentar</span>
+      {candidateState.loading ||
+        (voterState.loading && (
+          <div className="fixed left-0 top-0 z-10 h-screen w-screen">
+            <div className="flex flex-col space-y-2 justify-center h-full w-full items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL2tq0IANwwvpD-dJ-YD8Zbe0Xeriw2h-mdw&usqp=CAU"
+                alt="circle loading"
+                className="w-36 h-36 object-contain"
+              />
+              <span className="text-sm capitalize">tunggu sebentar</span>
+            </div>
           </div>
-        </div>
-      )}
-      {
-        !candidateState.loading && !voterState.loading && !userState.loading && 
+        ))}
+      {!candidateState.loading && !voterState.loading && !userState.loading && (
         <div>
-           <div className="my-2">
+          <div className="my-2">
             <ClockComponent />
             <div className="py-2">
               <span className="text-lg font-bold text-blue-800">
-                Total Suara Masuk : {voterState.data && voterState.data.length }
+                Total Suara Masuk : {voterState.data && voterState.data.length}
               </span>
             </div>
           </div>
 
-          {
-            voterState.error && voterState.message.includes("voter not found") || !currentVoter ?  <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-8">
-            {candidateState.data && candidateState.data.map((candidate, index) => {
-                return (
-                  <CardComponent
-                    buttonText={'Vote'}
-                    name={candidate.name}
-                    occupation={candidate.occupation}
-                    periode={candidate.periode}
-                    image={candidate.image}
-                    key={index}
-                    buttonClick={(e) => voteHandler(e, candidate.name)}
-                  />
-                );
-              })}
-            </div> 
-            :
-              <div className='my-4'> 
-                  <div className='text-center'>
-                    <span className='text-md text-red-600 font-bold'>
-                      Anda Sudah Memilih  
-                    </span>
-                  </div>  
-                  <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-8">
-                  </div>              
+          {(voterState.error &&
+            voterState.message.includes('voter not found')) ||
+          !currentVoter ? (
+            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-8">
+              {candidateState.data &&
+                candidateState.data.map((candidate, index) => {
+                  return (
+                    <CardComponent
+                      buttonText={'Vote'}
+                      name={candidate.name}
+                      occupation={candidate.occupation}
+                      periode={candidate.periode}
+                      image={candidate.image}
+                      key={index}
+                      buttonClick={(e) => voteHandler(e, candidate.name)}
+                    />
+                  );
+                })}
+            </div>
+          ) : (
+            <div className="my-4">
+              <div className="text-center">
+                <span className="text-md text-red-600 font-bold">
+                  Anda Sudah Memilih
+                </span>
               </div>
-          }
-        
-         
-        
+              <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-8"></div>
+            </div>
+          )}
         </div>
-      }
+      )}
     </div>
   );
 }
