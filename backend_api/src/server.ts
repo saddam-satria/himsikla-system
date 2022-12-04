b63c0bf8c8e70913d4b19fb03630f176ca545d79
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 <ardhisaddam@gmail.com>
2022-12-04 08:08:39 +0700

    updatae remove cors

--- backend_api/src/server.ts
+++ backend_api/src/server.ts
@@ -12,9 +12,7 @@ import prisma from './app/config/prisma';
   const app = express();
 
   app.use(
-    cors({
-      origin: ['https://pemilu.himsikaliabang.com', 'http://pemilu.himsikaliabang.com', "http://localhost:3000"],
-    })
+    cors()
   );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
