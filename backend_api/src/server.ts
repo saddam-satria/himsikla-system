9e913ef67b7b0d939a732d30a9ebff0c414f47ff
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 <ardhisaddam@gmail.com>
2022-12-04 08:10:38 +0700

    allow local env

--- backend_api/src/server.ts
+++ backend_api/src/server.ts
@@ -12,7 +12,9 @@ import prisma from './app/config/prisma';
   const app = express();
 
   app.use(
-    cors()
+    cors({
+      origin: ['https://pemilu.himsikaliabang.com', 'http://pemilu.himsikaliabang.com', "http://localhost:3000"],
+    })
   );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
